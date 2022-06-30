c7d949d0734d66243a585c5b83578fcf030e8253
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-29 22:13:52 -0600

    Strong type an array and use tuple for arrays

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 async function hello() {
     return 'world';
 }
+const arr2 = [1, 'Hi', false]; // fixed length array with strict types
+// STRONG TYPE AN ARRAY
+const arr = [];
+arr.push(1);
+// arr.push('23') ERROR
+// arr.push(false) ERROR
 // CUSTOM TYPE A FUNCTION
 function pow(x, y) {
     return Math.pow(x, y).toString();
--- index.ts
+++ index.ts
@@ -4,6 +4,16 @@ async function hello() {
   return 'world'
 }
 
+// TUPLE
+type MyList = [number, string, boolean, boolean?] // its presence can be optional using '?'
+const arr2: MyList = [1, 'Hi', false] // fixed length array with strict types
+
+// STRONG TYPE AN ARRAY
+const arr: number[] = [];
+arr.push(1)
+// arr.push('23') ERROR
+// arr.push(false) ERROR
+
 // CUSTOM TYPE A FUNCTION
 function pow(x: number, y: number): string /* or void if there is no return */ {
   return Math.pow(x, y).toString();
