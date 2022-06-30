e4697eedc62dc7b2cf7e39e7e1bd9e8910757f53
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-29 21:51:13 -0600

    Add custom type to a variable

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-console.log("hello");
 async function hello() {
     return 'world';
 }
+let font;
+font = 'bold';
+// font = 'something' gives out an error
+font = 23;
+// OPT OUT OF THE TYPE SYSTEM
+let lucky = 23;
+// let lucky; inferred as 'any' aswell
+lucky = '23';
 export {};
--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
-console.log("hello");
-
 import * as _ from 'lodash';
 
 async function hello() {
   return 'world'
 }
+
+// STRONG TYPE A VARIABLE
+type Style = 'bold' | 'italic' | 23;
+let font: Style;
+font = 'bold'
+// font = 'something' gives out an error
+font = 23
+
+// OPT OUT OF THE TYPE SYSTEM
+let lucky: any = 23
+// let lucky; inferred as 'any' aswell
+lucky = '23'
